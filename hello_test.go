014532971a6db177c30dd2121fe910d5deb239df
Venky Narayanan <venkyraghav@gmail.com>
2024-02-19 08:13:28 -0500

    Refactored test

--- hello_test.go
+++ hello_test.go
@@ -4,20 +4,16 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, Hello("Chris"), "Hello, Chris")
 	})
 	t.Run("say 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, World!"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-
+		assertCorrectMessage(t, Hello(""), "Hello, World")
 	})
+}
+
+func assertCorrectMessage(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
