637ab3bcadba4e8ce7f7a1f1168b05b1ec5ce13e
Venky Narayanan <venkyraghav@gmail.com>
2024-02-19 08:24:48 -0500

    Added language support

--- hello_test.go
+++ hello_test.go
@@ -3,11 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("saying hello to people", func(t *testing.T) {
-		assertCorrectMessage(t, Hello("Chris"), "Hello, Chris")
+	t.Run("saying hello to people in English", func(t *testing.T) {
+		assertCorrectMessage(t, Hello("Chris", english), "Hello, Chris")
 	})
-	t.Run("say 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
-		assertCorrectMessage(t, Hello(""), "Hello, World")
+	t.Run("say 'Hello, World!' in English when an empty string is supplied", func(t *testing.T) {
+		assertCorrectMessage(t, Hello("", english), "Hello, World")
+	})
+	t.Run("saying hello to people in Spanish", func(t *testing.T) {
+		assertCorrectMessage(t, Hello("Chris", spanish), "Hola, Chris")
+	})
+	t.Run("say 'Hello, World!' in Spanish when an empty string is supplied", func(t *testing.T) {
+		assertCorrectMessage(t, Hello("", spanish), "Hola, World")
+	})
+	t.Run("saying hello to people in French", func(t *testing.T) {
+		assertCorrectMessage(t, Hello("Chris", french), "Bonjour, Chris")
+	})
+	t.Run("say 'Hello, World!' in French when an empty string is supplied", func(t *testing.T) {
+		assertCorrectMessage(t, Hello("", french), "Bonjour, World")
 	})
 }
 
--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const english = "English"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, lang string) string {
+	prefix := englishHelloPrefix
+	if lang == spanish {
+		prefix = spanishHelloPrefix
+	} else if lang == french {
+		prefix = frenchHelloPrefix
+	}
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", english))
 }
